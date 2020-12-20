d1b67d02b3789fb3ccb6e5fb62af4687cfba7c67
hongyeongjune <hongyeongjune@naver.com>
2020-12-20 15:59:58 +0900

    Management By ErrorCode EnumType

---
+++ src/main/java/com/example/demo/commons/errors/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.example.demo.commons.errors;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    //Member
+    MEMBER_CANNOT_FOUND(400, "Can't found member"),
+
+    //Club
+    CLUB_CANNOT_FOUND(400, "Can't found Club"),
+
+    //Club Member
+    MEMBER_CANNOT_FOUND_IN_CLUB(400, "Can't found member in club");
+
+    private final int code;
+    private final String message;
+}
