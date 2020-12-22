b3efcc4e7205e2c0fedc6c1a15f84d94cc9cb0de
hongyeongjune <hongyeongjune@naver.com>
2020-12-22 17:16:09 +0900

    Community Dtos Design

--- src/main/java/com/example/demo/domain/community/dtos/CommunityUpdateDto.java
+++ src/main/java/com/example/demo/domain/community/dtos/CommunityUpdateDto.java
@@ -1,17 +1,11 @@
 package com.example.demo.domain.community.dtos;
 
-import com.example.demo.domain.club.dtos.ClubMemberSaveDto;
-import com.example.demo.domain.club.dtos.ClubSaveDto;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
-public class CommunitySaveDto {
-
+public class CommunityUpdateDto {
     private String title;
     private String content;
-    private int like;
 
     private String clubName;
     private String clubCity;
