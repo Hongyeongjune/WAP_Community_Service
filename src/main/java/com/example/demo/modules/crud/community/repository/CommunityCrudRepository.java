0211712acc9e6b7413eecaa07048fcfcc0efb397
hongyeongjune <hongyeongjune@naver.com>
2020-12-22 22:36:02 +0900

    Community Repository Design

--- src/main/java/com/example/demo/modules/crud/community/repository/CommunityCrudRepository.java
+++ src/main/java/com/example/demo/modules/crud/community/repository/CommunityCrudRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo.modules.crud.community.repository;
 
 import com.example.demo.modules.crud.club.entitys.Club;
-import com.example.demo.modules.crud.club.entitys.ClubMember;
 import com.example.demo.modules.crud.community.entitiys.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CommunityCrudRepository extends JpaRepository<Community, Long> {
-    Optional<Community> findByClubAndClubMember(Club club, ClubMember clubMember);
+    Optional<Community> findByClub(Club club);
 }
