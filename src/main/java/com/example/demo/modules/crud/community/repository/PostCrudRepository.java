2b05400f70fa6f5563b01e1a153486da867af214
hongyeongjune <hongyeongjune@naver.com>
2020-12-22 22:36:14 +0900

    Post Repository Design

--- src/main/java/com/example/demo/modules/crud/community/repository/PostCrudRepository.java
+++ src/main/java/com/example/demo/modules/crud/community/repository/PostCrudRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.modules.crud.community.repository;
 
 import com.example.demo.modules.crud.community.entitiys.Post;
+import com.example.demo.modules.crud.member.entitys.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostCrudRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findByPostIdAndMember(Long postId, Member member);
+    void deleteByPostIdAndMember(Long postId, Member member);
 }
