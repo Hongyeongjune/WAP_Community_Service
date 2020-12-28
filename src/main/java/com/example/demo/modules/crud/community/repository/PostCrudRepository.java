d81a81eb698a0733344dd5837b0e6a0f7f4ee77b
hongyeongjune <hongyeongjune@naver.com>
2020-12-28 16:30:50 +0900

    Post Repository Added a function to find posts

--- src/main/java/com/example/demo/modules/crud/community/repository/PostCrudRepository.java
+++ src/main/java/com/example/demo/modules/crud/community/repository/PostCrudRepository.java
@@ -1,13 +1,16 @@
 package com.example.demo.modules.crud.community.repository;
 
+import com.example.demo.modules.crud.community.entitiys.Community;
 import com.example.demo.modules.crud.community.entitiys.Post;
 import com.example.demo.modules.crud.member.entitys.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostCrudRepository extends JpaRepository<Post, Long> {
 
     Optional<Post> findByPostIdAndMember(Long postId, Member member);
+    Optional<List<Post>> findByCommunity(Community community);
     void deleteByPostIdAndMember(Long postId, Member member);
 }
