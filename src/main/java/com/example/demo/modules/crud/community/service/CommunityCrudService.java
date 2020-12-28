fe9a98c9a75258243cfd6873e10be9889fcd3ac1
hongyeongjune <hongyeongjune@naver.com>
2020-12-28 16:32:13 +0900

    Community CRUD Service Save, Find, Delete implementation

--- src/main/java/com/example/demo/modules/crud/community/service/CommunityCrudService.java
+++ src/main/java/com/example/demo/modules/crud/community/service/CommunityCrudService.java
@@ -1,11 +1,22 @@
 package com.example.demo.modules.crud.community.service;
 
+import com.example.demo.commons.errors.exception.ClubNotFoundException;
+import com.example.demo.commons.errors.exception.CommunityNotFoundException;
+import com.example.demo.commons.errors.exception.MemberNotFoundException;
+import com.example.demo.modules.crud.club.entitys.Club;
 import com.example.demo.modules.crud.club.repository.ClubCrudRepository;
 import com.example.demo.modules.crud.club.repository.ClubMemberCrudRepository;
+import com.example.demo.modules.crud.community.dtos.CommunityFindDto;
+import com.example.demo.modules.crud.community.dtos.CommunitySaveDto;
+import com.example.demo.modules.crud.community.entitiys.Community;
+import com.example.demo.modules.crud.community.entitiys.Post;
 import com.example.demo.modules.crud.community.repository.CommunityCrudRepository;
+import com.example.demo.modules.crud.member.entitys.Member;
 import com.example.demo.modules.crud.member.repository.MemberCrudRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,33 +27,56 @@ public class CommunityCrudService {
     private final ClubMemberCrudRepository clubMemberCrudRepository;
     private final MemberCrudRepository memberCrudRepository;
 
-//    public Community save(CommunitySaveDto dto) {
-//
-//        Club club = clubCrudRepository.findByClubNameAndCityAndDistrict(
-//                dto.getClubName(),
-//                dto.getClubCity(),
-//                dto.getClubDistrict()
-//        ).orElseThrow(ClubNotFoundException::new);
-//
-//        Member member = memberCrudRepository.findById(
-//                dto.getMemberId()
-//        ).orElseThrow(MemberNotFoundException::new);
-//
-//        ClubMember clubMember = clubMemberCrudRepository.findByClubAndMember(
-//                club,
-//                member
-//        ).orElseThrow(ClubMemberNotFoundException::new);
-//
-//        return communityCrudRepository.save(
-//                Community.builder()
-//                .title(dto.getTitle())
-//                .content(dto.getContent())
-//                .like(dto.getLike())
-//                .club(club)
-//                .clubMember(clubMember)
-//                .build()
-//        );
-//
-//    }
+    /**
+     * Apps Layer -> 성능 개선 예정
+     *
+     * 개선 해야 할 서비스 :
+     *      JWT 토큰 사용 -> 인증 방식 간소화 및 보안 강화
+     *      QueryDsl, JPQL, @Query 사용하여 데이터베이스 접근 최소화
+     *      Post 게시물 페이징 처리
+     */
+
+    public Community save(CommunitySaveDto dto) {
+
+        Club club = clubCrudRepository.findByClubNameAndCityAndDistrict(
+                dto.getClubName(),
+                dto.getClubCity(),
+                dto.getClubDistrict()
+        ).orElseThrow(ClubNotFoundException::new);
+
+        return communityCrudRepository.save(
+                Community.builder()
+                        .club(club)
+                        .build()
+        );
+    }
+
+    public Community find(CommunityFindDto dto) {
+
+        Club club = clubCrudRepository.findByClubNameAndCityAndDistrict(
+                dto.getClubName(),
+                dto.getClubCity(),
+                dto.getClubDistrict()
+        ).orElseThrow(ClubNotFoundException::new);
+
+        return communityCrudRepository.findByClub(
+                club
+        ).orElseThrow(CommunityNotFoundException::new);
+
+    }
+
+    public void delete(CommunityFindDto dto) {
+        Club club = clubCrudRepository.findByClubNameAndCityAndDistrict(
+                dto.getClubName(),
+                dto.getClubCity(),
+                dto.getClubDistrict()
+        ).orElseThrow(ClubNotFoundException::new);
+
+        Community community = communityCrudRepository.findByClub(
+                club
+        ).orElseThrow(CommunityNotFoundException::new);
+
+        communityCrudRepository.delete(community);
+    }
 
 }
