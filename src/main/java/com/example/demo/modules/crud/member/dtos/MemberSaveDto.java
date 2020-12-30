c99850ed2e455c67cbf3892c552b1fbfb9ef4003
hongyeongjune <hongyeongjune@naver.com>
2020-12-30 22:02:41 +0900

    Apps Layer Member Dtos Design

---
+++ src/main/java/com/example/demo/modules/crud/member/dtos/MemberSaveDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.modules.crud.member.dtos;
+
+import com.example.demo.commons.enums.DisclosureScopeState;
+import com.example.demo.commons.enums.MemberType;
+import com.example.demo.commons.enums.PositionType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberSaveDto {
+    private String id;
+    private String password;
+    private String name;
+    private String city;
+    private String district;
+    private String pictureUri;
+    private PositionType position;
+    private MemberType type;
+    private DisclosureScopeState disclosureScopeState;
+}
